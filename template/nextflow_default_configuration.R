99bfd6bc7024f20c4a7a4674e8c0fc73060981b8
Ari Ransfield <aransfield9@gmail.com>
2022-07-15 11:12:17 -0600

    Revert "- Updating license"
    This reverts commit 90fd0d03dc0f39b6f48f10d1f1e18cd56aec2d08.

--- template/nextflow_default_configuration.R
+++ template/nextflow_default_configuration.R
@@ -3,5 +3,4 @@ vcfs_path <- "./"
 autostart_supercronic <- FALSE
 minimum_required_variant_in_vcf_count <- 1000
 modules_to_compute <- "ethnicity,AllDiseases"
-verbose <- 1
-submission_logs_path <- "./"
+verbose <- 3
